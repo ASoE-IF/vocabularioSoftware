4b4e9569fa44546aa3779fc9d26d22bb24990493
DhiegoV <dhiegovinicius1@gmail.com>
2017-08-06 20:04:10 -0300

    colocando código do VE do JS de Anderson organizado

---
+++ VE_javaScript/src/packageTeste/MainTest.java
@@ -0,0 +1,47 @@
+package packageTeste;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.wst.jsdt.core.dom.AST;
+import org.eclipse.wst.jsdt.core.dom.ASTParser;
+import org.eclipse.wst.jsdt.core.dom.Block;
+import org.eclipse.wst.jsdt.core.dom.FunctionDeclaration;
+import org.eclipse.wst.jsdt.core.dom.JavaScriptUnit;
+import org.eclipse.wst.jsdt.core.dom.SingleVariableDeclaration;
+import org.eclipse.wst.jsdt.core.dom.VariableDeclarationStatement;
+
+public class MainTest {
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args){
+		List<VariableDeclarationStatement> functionVariables;
+		List<SingleVariableDeclaration> functionParameters = null;
+		Block functionBody = null;
+		String sourceCode = new String("function hello(argument1,argument2){var1;};");
+		
+		// configurando parser
+		ASTParser parser = ASTParser.newParser(AST.JLS3);
+		parser.setSource(sourceCode.toCharArray());
+		// criando AST
+		JavaScriptUnit unit = (JavaScriptUnit) parser.createAST(new NullProgressMonitor());
+		
+		List<FunctionDeclaration> node = unit.statements();
+		System.out.println("function name: " + node.get(0).getName());
+		
+		functionParameters = node.get(0).parameters();
+		
+		// imprimindo parâmetros
+		for(int j=0; j<functionParameters.size(); j++) { 
+			System.out.println("argument "+j+": " + functionParameters.get(j).getName());
+		}
+
+		functionBody = node.get(0).getBody();
+		functionVariables = functionBody.statements();
+		// imprimindo variáveis da função
+		for (int o=0; o<functionVariables.size(); o++)
+			System.out.println("functions variables: " + functionVariables.get(o));			  
+	}
+}
+				 
+				 
+
