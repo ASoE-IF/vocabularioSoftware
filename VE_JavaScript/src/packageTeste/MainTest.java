23681d1ec6de9aaeef9d23422e4415898a49c485
DhiegoV <dhiegovinicius1@gmail.com>
2017-12-13 13:10:08 -0300

    linhas de MainTest.java formatadas corretamente

--- VE_JavaScript/src/packageTeste/MainTest.java
+++ VE_JavaScript/src/packageTeste/MainTest.java
@@ -18,53 +18,49 @@ import org.eclipse.wst.jsdt.core.dom.VariableDeclarationStatement;
 
 public class MainTest {
 	@SuppressWarnings("unchecked")
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		StringBuffer sourceCode = new StringBuffer();
-        try {
-                InputStream stream = new FileInputStream("src/packageTeste/teste.js");
-                BufferedReader in = new BufferedReader(new InputStreamReader(stream));
+		try {
+			InputStream stream = new FileInputStream("src/packageTeste/teste.js");
+			BufferedReader in = new BufferedReader(new InputStreamReader(stream));
 
-                String line = "";
-                while (line != null) {
-                        line = in.readLine();
-                        sourceCode.append(line+"\n");
-                }
-                in.close();
-        } catch(IOException ioerr) {
-                System.out.println("Erro de I/O: " + ioerr);
-        }
+			String line = "";
+			while (line != null) {
+				line = in.readLine();
+				sourceCode.append(line + "\n");
+			}
+			in.close();
+		} catch (IOException ioerr) {
+			System.out.println("Erro de I/O: " + ioerr);
+		}
 
-        // configurando parser
-        ASTParser parser = ASTParser.newParser(AST.JLS3);
-        parser.setSource(sourceCode.toString().toCharArray());
+		// configurando parser
+		ASTParser parser = ASTParser.newParser(AST.JLS3);
+		parser.setSource(sourceCode.toString().toCharArray());
 
 		// criando AST
 		JavaScriptUnit astRoot = (JavaScriptUnit) parser.createAST(new NullProgressMonitor());
-		
+
 		// imprimindo informações de cada nó
-		for(ASTNode node : astRoot.statements()) {
+		for (ASTNode node : astRoot.statements()) {
 			if (node.getNodeType() == ASTNode.FUNCTION_DECLARATION) {
 				System.out.println("\nFunção encontrada:");
-				
+
 				FunctionDeclaration function = (FunctionDeclaration) node;
-				
-				System.out.println("nome: \n"
-				+ "\t"+function.getMethodName());
-				
+
+				System.out.println("nome: \n" + "\t" + function.getMethodName());
+
 				System.out.println("parâmetros:");
-				for(SingleVariableDeclaration parameter : (List<SingleVariableDeclaration>) function.parameters())
-					System.out.println("\t"+parameter.getName());
-				
+				for (SingleVariableDeclaration parameter : (List<SingleVariableDeclaration>) function.parameters())
+					System.out.println("\t" + parameter.getName());
+
 				System.out.println("variáveis:");
 				for (VariableDeclarationStatement statement : (List<VariableDeclarationStatement>) function.getBody().statements())
-					System.out.println("\t"+statement);
-				
+					System.out.println("\t" + statement);
+
 			} else {
 				System.out.println("\nEstrutura ainda não conhecida encontrada");
 			}
 		}
 	}
 }
-				 
-				 
-
