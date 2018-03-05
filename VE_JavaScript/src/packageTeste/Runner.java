9ae176799fac4a0027cbed8fe576525ef2713b94
DhiegoV <dhiegovinicius1@gmail.com>
2018-03-05 19:34:17 -0300

    adicionado diagrama de classe do estado atual e reorganizado FileParser
    A reorganização realizada na classe FileParser foi simples: seus dois métodos foram
    1) renomeados para nomes melhores e;
    2) permutados, deixando o 'mais importante' (parseFile) acima.

--- VE_JavaScript/src/packageTeste/Runner.java
+++ VE_JavaScript/src/packageTeste/Runner.java
@@ -5,6 +5,6 @@ import packageTeste.util.FileParser;
 
 public class Runner {
 	public static void main(String[] args) {
-		FileParser.parse("src/packageTeste/teste.js");
+		FileParser.parseFile("src/packageTeste/teste.js");
 	}
 }
--- VE_JavaScript/src/packageTeste/util/FileParser.java
+++ VE_JavaScript/src/packageTeste/util/FileParser.java
@@ -14,37 +14,37 @@ import org.eclipse.wst.jsdt.core.dom.JavaScriptUnit;
 import packageTeste.processors.ASTRootProcessor;
 
 public class FileParser {
-	
-	public static char[] read(String filePath) {
+
+	public static void parseFile(String filePath) {
+		char[] sourceCode = readFile(filePath);
+
+		// configurando parser
+		ASTParser parser = ASTParser.newParser(AST.JLS3);
+		parser.setSource(sourceCode);
+
+		// criando AST
+		JavaScriptUnit astRoot = (JavaScriptUnit) parser.createAST(new NullProgressMonitor());
+
+		ASTRootProcessor.process(astRoot);
+	}
+
+	public static char[] readFile(String filePath) {
 		StringBuffer sourceCode = new StringBuffer();
-		
+
 		try {
 			InputStream stream = new FileInputStream(filePath);
 			BufferedReader in = new BufferedReader(new InputStreamReader(stream));
 			String line = "";
-			
+
 			while (null != (line = in.readLine()))
 				sourceCode.append(line + "\n");
-			
+
 			in.close();
-			
+
 		} catch (IOException ioerr) {
 			System.out.println(ioerr);
 		}
-		
+
 		return sourceCode.toString().toCharArray();
-	}
-	
-	public static void parse(String filePath) {
-		char[] sourceCode = read(filePath);
-		
-		// configurando parser
-		ASTParser parser = ASTParser.newParser(AST.JLS3);
-		parser.setSource(sourceCode);
-	
-		// criando AST
-		JavaScriptUnit astRoot = (JavaScriptUnit) parser.createAST(new NullProgressMonitor());
-		
-		ASTRootProcessor.process(astRoot);
 	}
 }
