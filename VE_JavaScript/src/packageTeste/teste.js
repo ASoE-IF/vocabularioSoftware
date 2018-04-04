d6594f6423c33d580598bf0f84038b6bb6cda77e
DhiegoV <dhiegovinicius1@gmail.com>
2018-04-04 09:07:07 -0300

    não mais usando uma função desnecessária para definir uma classe e modificado método de saída de dados

--- VE_JavaScript/src/packageTeste/teste.js
+++ VE_JavaScript/src/packageTeste/teste.js
@@ -2,34 +2,29 @@
 // declarando construtores de objeto (tipos)
 
 // utilizando prototipos
-var Casa = function() {
-    // constructor
-    function Casa() {
-        this._nome = null;
-        this._dono = null;
-        this._valor = null;
-    };
+function Casa() {
+    this._nome = null;
+    this._dono = null;
+    this._valor = null;
+}
 
-    Casa.prototype.getDono = function() {
-        return this._dono;
-    };
-    Casa.prototype.setDono = function(dono) {
-        this._dono = dono;
-    };
-    Casa.prototype.getValor = function() {
-        return this._valor;
-    };
-    Casa.prototype.setValor = function(valor) {
-        this._valor = valor;
-    };
-    Casa.prototype.getNome = function() {
-        return this._nome;
-    };
-    Casa.prototype.setNome = function(nome) {
-        this._nome = nome;
-    };
-
-    return Casa;
+Casa.prototype.getDono = function() {
+    return this._dono;
+};
+Casa.prototype.setDono = function(dono) {
+    this._dono = dono;
+};
+Casa.prototype.getValor = function() {
+    return this._valor;
+};
+Casa.prototype.setValor = function(valor) {
+    this._valor = valor;
+};
+Casa.prototype.getNome = function() {
+    return this._nome;
+};
+Casa.prototype.setNome = function(nome) {
+    this._nome = nome;
 };
 
 // utilizando unicas funcoes
@@ -65,27 +60,26 @@ casaBranca.setNome("Casa Branca");
 casaBranca.setDono("Donald Trump");
 casaBranca.setValor(100);
 
-alert(casaBranca.getDono() + " é dono\n" + "da casa de nome " + casaBranca.getNome() + ", ela vale " + casaBranca.getValor());
+console.log(casaBranca.getDono()+" é dono da casa de nome "+casaBranca.getNome()+", ela vale "+casaBranca.getValor());
 
 var bacia = new Recipiente(50);
 
 bacia.encher(25, "água");
-alert("a bacia tem " + bacia.volConteudo +
-    " de " + bacia.nomeConteudo);
+console.log("a bacia tem "+bacia.volConteudo+"L de "+bacia.nomeConteudo);
 
 var removido = bacia.remover(20);
-alert("tirei " + removido + "da bacia\n" + "agora ela tem " + bacia.volConteudo + " de " + bacia.nomeConteudo);
+console.log("tirei "+removido+"L da bacia, agora ela tem "+bacia.volConteudo+"L de "+bacia.nomeConteudo);
 
 // funcao exemplo
 
 function juntarStrings(string1, string2) {
     return string1 + string2;
-};
+}
 
 var valor1 = prompt("Digite a primeira string, pessoa");
 var valor2 = prompt("Digite a segunda string, pessoa");
 
 var juncao = juntarStrings(valor1, valor2);
 
-alert("A junção deu \"" + juncao + "\"");
+console.log("A junção deu \"" + juncao + "\"");
 
